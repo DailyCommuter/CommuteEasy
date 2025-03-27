30f876fd098590453c2bb07b02311aa688f4bfe3
boruchk <boruk770@gmail.com>
2025-03-26 20:32:47 -0400

    update db schema for better data management into separate tables (normalize data based on its type

--- appname/schema.sql
+++ appname/schema.sql
@@ -3,30 +3,45 @@ DROP TABLE IF EXISTS post;
 
 
 CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
 
 CREATE TABLE subway_alerts (
-  alert_id TEXT,
-  agency_id TEXT,
-  route_id INTEGER,
-  alert_text TEXT,
-  description_text TEXT
+    alert_id TEXT,
+    agency_id TEXT,
+    route_id INTEGER,
+    alert_text TEXT,
+    description_text TEXT
 );
 
 
 CREATE TABLE trip_update (
-  update_id TEXT,
-  trip_id TEXT,
-  start_tm TEXT,
-  start_dt TEXT,
-  route_id TEXT,
-  arrival INTEGER,
-  departure INTEGER,
-  stop_id TEXT,
-  timestmp INTEGER,
-  curr_stop_id TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    update_id TEXT NOT NULL UNIQUE,
+    trip_id TEXT,
+    start_tm TEXT,
+    start_dt TEXT,
+    route_id TEXT
+);
+
+
+CREATE TABLE stop_update (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trip_update_id INTEGER NOT NULL,
+    arrival INTEGER,
+    departure INTEGER,
+    stop_id TEXT,
+    FOREIGN KEY (trip_update_id) REFERENCES trip_update(id)
+);
+
+
+CREATE TABLE vehicle_update (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trip_update_id INTEGER NOT NULL,
+    timestmp INTEGER,
+    curr_stop_id TEXT,
+    FOREIGN KEY (trip_update_id) REFERENCES trip_update(id)
 );

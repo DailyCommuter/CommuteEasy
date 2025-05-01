071934deffcfb65a7a965cca1f348d8c3ff29866
Emmanuel Owusu-Ampaw <118029142+emmanueloa503@users.noreply.github.com>
2025-04-30 22:12:17 -0400

    Set up Firebase Auth context, ProtectedRoute, and secured routes

--- DailyCommuterFrontend/client/daily-commuter/src/App.jsx
+++ DailyCommuterFrontend/client/daily-commuter/src/App.jsx
@@ -4,13 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/welcome-page";
 import HomePage from "./pages/home";
 import CommutingPage from "./pages/commuting-page";
+import ProtectedRoute from "./context/ProtectedRoute";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/commuting" element={<CommutingPage />} />
+        
+        {/* Protected Routes*/}
+        <Route path="/home" element={<ProtectedRoute> <HomePage /> </ProtectedRoute>} />
+        <Route path="/commuting" element={<ProtectedRoute> <CommutingPage /> </ProtectedRoute>} />
 
         {/* Catch-all route for 404s */}
         {/* <Route path="*" element={<NotFound />} /> */}
